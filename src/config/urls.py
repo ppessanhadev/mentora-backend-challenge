596a279f751dc4c2ecdb3a78c2cda5608ffe2384
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-25 08:35:26 -0300

    feat: initial configuration

---
+++ src/config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from ninja import NinjaAPI
+
+from src.modules.mentoring.handlers import router as mentoring_router
+
+api = NinjaAPI(
+    title="Mentora backend", description="This is mentora's challenge API RESTFUL"
+)
+
+api.add_router("mentoring", mentoring_router)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),
+]
