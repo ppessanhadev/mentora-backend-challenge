1c2544897499b897f4ef7b14c9583ce4ebb90f55
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-28 18:44:07 -0300

    refat: checks if exists mentoring or throw an 404 error

--- src/modules/mentoring/use_cases/get_by_id.py
+++ src/modules/mentoring/use_cases/get_by_id.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from ..models import MentoringModel
 
 
 def get_by_id_use_case(id: int):
-    mentoring = MentoringModel.objects.get(id=id)
+    mentoring = get_object_or_404(MentoringModel, id=id)
     return mentoring
