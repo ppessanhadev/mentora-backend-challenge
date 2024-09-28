1dda563e12c57e40949ecdd06d9dec3d1e3dc5ef
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-28 11:50:57 -0300

    feat: adds throttling resquests rate limit

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+from ninja.throttling import AnonRateThrottle
 
 from src.modules.mentoring.handlers import router as mentoring_router
 
 api = NinjaAPI(
-    title="Mentora backend", description="This is mentora's challenge API RESTFUL"
+    title="Mentora backend",
+    description="This is mentora's challenge API RESTFUL",
+    throttle=[AnonRateThrottle("8/s")],
 )
 
 
